4a22302fd62d0a41f5600c4acc7ed406d3a263d1
˹ᴋʀɪsʜɴᴀ ˼ <officialkrishna7618@gmail.com>
2023-08-19 11:55:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,12 @@
-
-#
-
 from os import getenv
-
 from dotenv import load_dotenv
-
 load_dotenv()
 
-# Get it from my.telegram.org
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
-## Get it from @Botfather in Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
+LOG_ID = int(getenv("LOG_ID", ""))
 
-# Database to save your chats and stats... Get MongoDB:-  https://notreallyshikhar.gitbook.io/AfkRobotmusicbot/deployment/mongodb#4.-youll-see-a-deploy-cloud-database-option.-please-select-shared-hosting-under-free-plan-here
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-
-# SUDO USERS
-SUDO_USER = list(
-    map(int, getenv("SUDO_USER", "").split())
-)  # Input type must be interger
+SUDO_USER = list(map(int, getenv("SUDO_USER", "").split()))
