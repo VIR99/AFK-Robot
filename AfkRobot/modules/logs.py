83e5a66121446c49c83414d0fa8184e47d30f035
˹ᴋʀɪsʜɴᴀ ˼ <officialkrishna7618@gmail.com>
2023-09-01 13:09:32 +0530

    Update logs.py

--- AfkRobot/modules/logs.py
+++ AfkRobot/modules/logs.py
@@ -7,20 +7,20 @@ async def new_message(chat_id: int, message: str):
     await app.send_message(chat_id=chat_id, text=message)
 
 @app.on_message(filters.new_chat_members)
-async def on_new_chat_members(message: Message):
+async def on_new_chat_members(_, message: Message):
     if (await app.get_me()).id in [user.id for user in message.new_chat_members]:
         added_by = message.from_user.mention if message.from_user else "ᴜɴᴋɴᴏᴡɴ ᴜsᴇʀ"
         title = message.chat.title
-        username = f"@{message.chat.username}"
+        username = f"@{message.chat.username}" if message.chat.username else "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
         chat_id = message.chat.id
-        Atiya = f"**✫** <b><u>ɴᴇᴡ ɢʀᴏᴜᴘ</u></b> **:**\n\n**ᴄʜᴀᴛ ɪᴅ :** {chat_id}\n**ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :** {username}\n**ᴄʜᴀᴛ ᴛɪᴛʟᴇ :** {title}\n\n**ᴀᴅᴅᴇᴅ ʙʏ :** {added_by}"
-        await new_message(LOG_ID, Atiya)
+        new = f"**✫** <b><u>ɴᴇᴡ ɢʀᴏᴜᴘ</u></b> **:**\n\n**ᴄʜᴀᴛ ɪᴅ :** {chat_id}\n**ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :** {username}\n**ᴄʜᴀᴛ ᴛɪᴛʟᴇ :** {title}\n\n**ᴀᴅᴅᴇᴅ ʙʏ :** {added_by}"
+        await new_message(LOG_ID, new)
 
 @app.on_message(filters.left_chat_member)
-async def on_left_chat_member(message: Message):
+async def on_left_chat_member(_, message: Message):
     if (await app.get_me()).id == message.left_chat_member.id:
         remove_by = message.from_user.mention if message.from_user else "ᴜɴᴋɴᴏᴡɴ ᴜsᴇʀ"
         title = message.chat.title
         chat_id = message.chat.id
-        goodbye = f"**✫** <b><u>ʟᴇғᴛ ɢʀᴏᴜᴘ</u></b> **:**\n\n**ᴄʜᴀᴛ ɪᴅ :** {chat_id}\n**ᴄʜᴀᴛ ᴛɪᴛʟᴇ :** {title}\n\n**ʀᴇᴍᴏᴠᴇᴅ ʙʏ :** {remove_by}"
-        await new_message(LOG_ID, goodbye)
+        left = f"**✫** <b><u>ʟᴇғᴛ ɢʀᴏᴜᴘ</u></b> **:**\n\n**ᴄʜᴀᴛ ɪᴅ :** {chat_id}\n**ᴄʜᴀᴛ ᴛɪᴛʟᴇ :** {title}\n\n**ʀᴇᴍᴏᴠᴇᴅ ʙʏ :** {remove_by}"
+        await new_message(LOG_ID, left)
